ac5d5797b5bd412613983a1d074a25fa080b61fa
Vittokm <vittokm@gmail.com>
2021-03-17 21:10:08 +0100

    vowel checker

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+
+function countV() {
+
+    let vwlNum = 0;
+    let vog1 = "a";
+    let vog2 = "e";
+    let vog3 = "i";
+    let vog4 = "o";
+    let vog5 = "u";
+    let txt = document.querySelector("#txt").value;
+
+    for (let i = 0; i < txt.length; i++) {
+
+        if (txt[i] === vog1 || txt[i] === vog2 || txt[i] === vog3 || txt[i] === vog4 || txt[i] === vog5) {
+            vwlNum++;
+        } 
+    }
+
+    document.querySelector("#txt").value = "";
+
+    alert("There are " + vwlNum + " vowels.");
+    
+}
